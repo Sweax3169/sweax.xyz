0d63d0246306fc10fd549d6782d6fa8a7aa55fdb
Sweax3169 <okanbaba.0188@gmail.com>
2025-10-12 08:49:58 +0300

    auto update 08:49:58

--- app/app.py
+++ app/app.py
@@ -12,5 +12,5 @@ def admin():
     return render_template("admin.html")
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port,debug=True)
 
