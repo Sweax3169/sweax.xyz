57f5774e9054b1563b50dab9af477b23444eb477
Sweax3169 <okanbaba.0188@gmail.com>
2025-10-12 12:01:02 +0300

    FOTO DEGST NDEX

--- app/app.py
+++ app/app.py
@@ -27,10 +27,7 @@ def girisadmin():
             return render_template('admingiris.html', hata="LA AMINAKODUMUN SALA MALMISIN SEN NİYE BIRAKIYON KULLANICI ADINI BOŞ")
         elif not sifre:
             return render_template('admingiris.html', hata='LAN BIRAKMASANA ŞİFRENİ BOŞ')
-# #if porno2(kullaniciad):
-# #    return render_template('giriş.html', hata="siktirla")
-# #elif porno2(şifre):
-# #    return render_template('giriş.html', hata="siktir la")
+
         else:
 
 
@@ -48,13 +45,6 @@ def girisadmin():
                 return redirect(url_for("admin"))
             else:
                 return render_template("admingiris.html",hata="GARDAŞ SENİN HESABIN YOK YA")
-#     if kullanıcı:
-#         session['kimlik'] = kimlik
-#         if kullanıcı[7]:
-#             session['hesapseviyesi'] = 1
-#         return redirect(url_for('index'))
-#     else:
-#         return render_template('admingiris.html', hata="kıllnıcı adı veya kimlikde bi takım sikintilar var")
 
     return render_template('admingiris.html')
 @app.route("/admin")
